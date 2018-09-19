81f22ff85a5e4e678da67eb3aa62d470dedaff09
txemacstylesage <jose.bermudez@stylesage.co>
2018-09-19 14:11:47 +0200

    Add while True and read line.

--- chatbot/main.py
+++ chatbot/main.py
@@ -5,10 +5,11 @@ import settings
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(settings.DATA_URL) as resp:
-            print(resp.status)
-            print(await resp.text())
+    while True:
+        input()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(settings.DATA_URL) as resp:
+                print(await resp.text())
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
